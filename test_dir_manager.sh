bd78f2eb71d0d320dd074d1508aa4e5e1a3c1932
pmonetti <pmonetti@forkworks.com>
2016-10-10 19:45:07 -0300

    Add new dir_manager.sh test.

--- test_dir_manager.sh
+++ test_dir_manager.sh
@@ -7,6 +7,7 @@ reset_test_dir()
     $SCRIPT_DIR/prepare_test_dir.sh
     rm -rf $TMP_DIR
     mkdir -p $TMP_DIR
+    tree $TEST_DIR --dirsfirst --noreport > $ORIGINAL_TREE
 }
 
 replace()
@@ -26,8 +27,10 @@ remove_line()
     cp $REPLACE_TMP_FILE $FILEPATH
 }
 
-rename_expected_tree_files()
+prepare_expected_output_1()
 {
+    cp $ORIGINAL_TREE $EXPECTED_TREE
+    replace $EXPECTED_TREE "└" "├"
     replace $EXPECTED_TREE ".AVI" ".avi"
     replace $EXPECTED_TREE ".EXE" ".exe"
     replace $EXPECTED_TREE "áéíóúñ" "aeioun"
@@ -36,24 +39,43 @@ rename_expected_tree_files()
     replace $EXPECTED_TREE "test dir" "test_dir"
 }
 
-remove_expected_tree_files()
+prepare_expected_output_2()
 {
+    cp $ORIGINAL_TREE $EXPECTED_TREE
+    replace $EXPECTED_TREE "└" "├"
     remove_line $EXPECTED_TREE ".EXE"
     remove_line $EXPECTED_TREE ".exe"
     remove_line $EXPECTED_TREE "testdir51"
 }
 
-move_expected_tree_files()
+prepare_expected_output_3()
 {
-    remove_line $EXPECTED_TREE ".EXE"
+    cp $ORIGINAL_TREE $EXPECTED_TREE
+    replace $EXPECTED_TREE "└" "├"
+    remove_line $EXPECTED_TREE ".AVI"
+    remove_line $EXPECTED_TREE ".avi"
+    remove_line $EXPECTED_TREE "testdir52"
+}
+
+prepare_expected_output_4()
+{
+    cp $ORIGINAL_TREE $EXPECTED_TREE
+    replace $EXPECTED_TREE "└" "├"
+    sed -i "s|$TEST_DIR|$OUTPUT_DIR|g" $EXPECTED_TREE
+    remove_line $EXPECTED_TREE ".txt"
     remove_line $EXPECTED_TREE ".exe"
+    remove_line $EXPECTED_TREE ".ini"
+    remove_line $EXPECTED_TREE ".EXE"
+    remove_line $EXPECTED_TREE "no_ext"
+    remove_line $EXPECTED_TREE ".xlextension"
+    remove_line $EXPECTED_TREE ".XLEXTENSION"   
     remove_line $EXPECTED_TREE "testdir51"
 }
 
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-OUTPUT_DIR=$SCRIPT_DIR/output
 TEST_DIR=$SCRIPT_DIR/testdir
+OUTPUT_DIR=$SCRIPT_DIR/output
 TMP_DIR=/tmp/dir_manager
 ORIGINAL_TREE=$TMP_DIR/original_tree.txt
 EXPECTED_TREE=$TMP_DIR/expected_tree.txt
@@ -62,33 +84,38 @@ REPLACE_TMP_FILE=$TMP_DIR/replace_tmp_file.txt
 
 
 # In $TEST_DIR, rename all extensions to lowercase, keep only english chars and replace all whitespaces with _
-echo "*********** Running Test 1***********"
+echo "*********** Running Test 1 ***********"
 reset_test_dir
-tree $TEST_DIR --dirsfirst --noreport > $ORIGINAL_TREE
-cp $ORIGINAL_TREE $EXPECTED_TREE
-rename_expected_tree_files
 $SCRIPT_DIR/dir_manager.sh -l -k -s _ $OUTPUT_DIR $TEST_DIR
-tree $TEST_DIR --dirsfirst --noreport > $OUTPUT_TREE
+prepare_expected_output_1
+tree $TEST_DIR --dirsfirst --noreport > $OUTPUT_TREE  && replace $OUTPUT_TREE "└" "├"
 diff $OUTPUT_TREE $EXPECTED_TREE > /dev/null || { echo '"*********** Test 1 Failed ***********"'; exit 1; }
 echo "*********** Test 1 Ok ***********"
 
+
 # In $TEST_DIR removes all .exe (case insensitive) files
-echo "*********** Running Test 2***********"
+echo "*********** Running Test 2 ***********"
 reset_test_dir
-tree $TEST_DIR --dirsfirst --noreport > $ORIGINAL_TREE
-cp $ORIGINAL_TREE $EXPECTED_TREE
-replace $EXPECTED_TREE "└" "├"
-remove_expected_tree_files
 $SCRIPT_DIR/dir_manager.sh -r exe $OUTPUT_DIR $TEST_DIR
-tree $TEST_DIR --dirsfirst --noreport > $OUTPUT_TREE
-replace $OUTPUT_TREE "└" "├"
+prepare_expected_output_2
+tree $TEST_DIR --dirsfirst --noreport > $OUTPUT_TREE && replace $OUTPUT_TREE "└" "├"
 diff $OUTPUT_TREE $EXPECTED_TREE > /dev/null || { echo '"*********** Test 2 Failed ***********"'; exit 1; }
 echo "*********** Test 2 Ok ***********"
 
 
 # In $TEST_DIR, move all .avi (case insensitive) files to $OUTPUT_DIR
-#reset_test_dir
-#$SCRIPT_DIR/dir_manager.sh -m avi $OUTPUT_DIR $TEST_DIR
+echo "*********** Running Test 3 ***********"
+reset_test_dir
+$SCRIPT_DIR/dir_manager.sh -m avi $OUTPUT_DIR $TEST_DIR
+
+prepare_expected_output_3
+tree $TEST_DIR --dirsfirst --noreport > $OUTPUT_TREE && replace $OUTPUT_TREE "└" "├"
+diff $OUTPUT_TREE $EXPECTED_TREE > /dev/null || { echo '"*********** Test 3 Failed ***********"'; exit 1; }
+
+prepare_expected_output_4
+tree $OUTPUT_DIR --dirsfirst --noreport > $OUTPUT_TREE && replace $OUTPUT_TREE "└" "├"
+diff $OUTPUT_TREE $EXPECTED_TREE > /dev/null || { echo '"*********** Test 3 Failed ***********"'; exit 1; }
+echo "*********** Test 3 Ok ***********"
 
 
 # In $TEST_DIR, rename all extensions to lowercase, keep only english chars, replace all whitespaces with _,
