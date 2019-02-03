0718c247f8cfb6273184fbc00a043d01b2c7d850
pmonetti <rodriguez.monetti@gmail.com>
2019-02-03 20:39:20 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,30 +1,31 @@
-#/bin/sh
+#!/bin/sh
 
-# Add the Spotify repository signing key to be able to verify downloaded packages
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
-echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+GOOGLE_INSTALLER_PATH=~/google-chrome-stable_current_amd64.deb
+
+# Validate that Google Chrome installer exists
+ls $GOOGLE_INSTALLER_PATH || exit
 
 # Install packages
 sudo apt-get update
 sudo apt-get install -y yakuake htop tree
-sudo apt-get install -y synaptic dconf-tools gksu gdebi inxi leafpad catfish pavucontrol 
-sudo apt-get install -y xubuntu-restricted-extras libdvd-pkg
 sudo apt-get install -y git geany openvpn
-sudo apt-get install -y libgconf2-4 libnss3-1d libxss1
-sudo apt-get install -y spotify-client
 
-sudo dpkg -i ~/Downloads/google-chrome-stable_current_amd64.deb
+sudo dpkg -i $GOOGLE_INSTALLER_PATH || exit 1
 sudo apt-get install -f -y
 
+if [ ! -f ~/Bash-Scripts/setup.sh ]; then
+    git clone https://github.com/pmonetti/Bash-Scripts.git || exit 1
+fi
+
 # Upgrade system and clean
-git clone https://github.com/pmonetti/Bash-Scripts.git
-~/Bash-Scripts/update_linux.sh
+~/Bash-Scripts/update_linux.sh || exit 1
 
 # Load XFCE panel
-~/Bash-Scripts/load_xfce_panel.sh
+~/Bash-Scripts/load_xfce_panel.sh || exit 1
 
 # Add yakuake to startup
-mkdir -p ~/.config/autostart/
+mkdir -p ~/.config/autostart/ || exit 1
+rm ~/.config/autostart/yakuake.desktop || exit 1
 cat << EOF >> ~/.config/autostart/yakuake.desktop
 [Desktop Entry]
 Type=Application
@@ -32,5 +33,3 @@ Exec=/usr/bin/yakuake
 Name=yakuake
 Comment=yakuake 
 EOF
-
-
