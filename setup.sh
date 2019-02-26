73124a7e9425096fb69aa4ae4f6cae92c4f21959
pmonetti <rodriguez.monetti@gmail.com>
2019-02-25 21:07:59 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,16 +3,23 @@
 GOOGLE_INSTALLER_PATH=~/google-chrome-stable_current_amd64.deb
 
 # Validate that Google Chrome installer exists
-ls $GOOGLE_INSTALLER_PATH || exit
+ls $GOOGLE_INSTALLER_PATH || exit 1
 
 # Install packages
-sudo apt-get update
-sudo apt-get install -y vlc yakuake htop tree \
-						git geany openvpn gparted
+sudo apt-get update || exit 1
+
+sudo apt-get install -y \
+						yakuake geany htop \
+						tree gparted baobab \
+						git \
+						vlc \
+						openvpn \
+						|| exit 1
 
 sudo dpkg -i $GOOGLE_INSTALLER_PATH || exit 1
-sudo apt-get install -f -y
+sudo apt-get install -f -y || exit 1
 
+# Clone bash scripts if they are not available
 if [ ! -f ~/Bash-Scripts/setup.sh ]; then
     git clone https://github.com/pmonetti/Bash-Scripts.git || exit 1
 fi
