58842573498561367642e49e945cf964c802449b
pmonetti <rodriguez.monetti@gmail.com>
2019-04-19 12:32:08 -0300

    Update get_system_settings.sh script

--- get_system_settings.sh
+++ get_system_settings.sh
@@ -5,7 +5,7 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $SCRIPT_DIR/utils.sh
 
-print_and_exec "inxi -ADGS"
-print_and_exec "uname -r"
-print_and_exec "lsb_release -a"
-print_and_exec "lscpu"
+print_and_exec "inxi -F"
+#print_and_exec "uname -r"
+#print_and_exec "lsb_release -a"
+#print_and_exec "lscpu"
