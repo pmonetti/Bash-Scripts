5f91c0cf193b896b70635edab1b2a959730f813e
pmonetti <rodriguez.monetti@gmail.com>
2019-04-20 00:06:08 -0300

    Improve panel

--- load_xfce_panel.sh
+++ load_xfce_panel.sh
@@ -26,7 +26,7 @@ cat << EOF >> $CONFIG_FILE_PATH
 /panels/panel-0/length uint32 100
 /panels/panel-0/length-adjust true
 /panels/panel-0/mode uint32 0
-/panels/panel-0/plugin-ids [<1>, <2>, <3>, <9>, <4>, <5>, <19>, <8>, <12>, <18>, <10>, <14>, <11>]
+/panels/panel-0/plugin-ids [<1>, <2>, <3>, <22>, <4>, <21>, <5>, <12>, <8>, <18>, <10>, <14>, <11>]
 /panels/panel-0/position 'p=6;x=0;y=0'
 /panels/panel-0/position-locked true
 /panels/panel-0/size uint32 24
@@ -35,30 +35,29 @@ cat << EOF >> $CONFIG_FILE_PATH
 /plugins/plugin-10 'xfce4-notes-plugin'
 /plugins/plugin-11 'actions'
 /plugins/plugin-11/appearance uint32 0
-/plugins/plugin-11/items [<'+lock-screen'>, <'-switch-user'>, <'-separator'>, <'-suspend'>, <'-hibernate'>, <'-separator'>, <'+shutdown'>, <'-restart'>, <'-separator'>, <'-logout'>, <'-logout-dialog'>]
+/plugins/plugin-11/items [<'+lock-screen'>, <'-switch-user'>, <'-separator'>, <'-suspend'>, <'-hibernate'>, <'-separator'>, <'+shutdown'>, <'+restart'>, <'-separator'>, <'-logout'>, <'-logout-dialog'>]
 /plugins/plugin-12 'weather'
 /plugins/plugin-14 'showdesktop'
 /plugins/plugin-18 'separator'
 /plugins/plugin-18/style uint32 3
-/plugins/plugin-19 'separator'
-/plugins/plugin-19/style uint32 3
 /plugins/plugin-2 'tasklist'
 /plugins/plugin-2/flat-buttons true
 /plugins/plugin-2/show-handle false
 /plugins/plugin-2/show-labels true
 /plugins/plugin-2/sort-order uint32 1
+/plugins/plugin-21 'systemload'
+/plugins/plugin-22 'pulseaudio'
+/plugins/plugin-22/enable-keyboard-shortcuts true
 /plugins/plugin-3 'separator'
 /plugins/plugin-3/expand true
 /plugins/plugin-3/style uint32 0
 /plugins/plugin-4 'systray'
-/plugins/plugin-4/names-visible [<'thunar'>, <'redshift-gtk'>, <'red'>, <'notificador de actualizaciones'>, <'terminal de xfce'>, <'guake'>, <'skype'>, <'fluxgui'>, <'blueman-applet'>, <'notas'>, <'miniaplicación gestor de la red'>, <'xfce4-power-manager'>, <'scp-dbus-service.py'>, <'javaembeddedframe'>, <'desplegar terminal'>]
+/plugins/plugin-4/names-visible [<'thunar'>, <'vlc'>, <'redshift-gtk'>, <'red'>, <'notificador de actualizaciones'>, <'terminal de xfce'>, <'networkmanager applet'>, <'task manager'>, <'guake'>, <'skype'>, <'network'>, <'fluxgui'>, <'blueman-applet'>, <'notas'>, <'update-notifier'>, <'miniaplicación gestor de la red'>, <'xfce4-power-manager'>, <'scp-dbus-service.py'>, <'javaembeddedframe'>, <'desplegar terminal'>]
 /plugins/plugin-4/show-frame false
 /plugins/plugin-4/size-max uint32 22
 /plugins/plugin-5 'power-manager-plugin'
 /plugins/plugin-8 'clock'
 /plugins/plugin-8/digital-format '%d %b, %H:%M'
-/plugins/plugin-9 'launcher'
-/plugins/plugin-9/items [<'14822583191.desktop'>]
 EOF
 
 cat << EOF >> $LAUNCHER_DESKTOP_FILE
@@ -88,7 +87,7 @@ if [ "$LINUX_VERSION_ID" = "16.04" ] ; then
 elif [ "$LINUX_VERSION_ID" = "18.04" ] ; then
 	xfpanel-switch load $TAR_FILE
 else
-	echo "Unsupported linux version:" $LINUX_VERSION_ID 
+	echo "Unsupported linux version:" $LINUX_VERSION_ID
 fi
 
 rm -rf $TMP_PANEL_DIR
