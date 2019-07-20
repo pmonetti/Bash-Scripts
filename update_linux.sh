31dfc6e04dd64ab0f8711f96e9e8f6017d3fc5b4
pmonetti <rodriguez.monetti@gmail.com>
2019-07-20 16:21:09 -0300

    Improve update_linux.sh to also upgrade packages 'kept-back'

--- update_linux.sh
+++ update_linux.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 sudo apt-get -y update || exit 1
-sudo apt-get -y upgrade || exit 1
+sudo apt-get -y --with-new-pkgs upgrade || exit 1
 sudo apt-get -y clean || exit 1
 sudo apt-get -f install || exit 1
 sudo apt-get -y clean || exit 1
 sudo apt-get -y autoclean || exit 1
 sudo apt-get -y autoremove || exit 1
-sudo apt-get check
+sudo apt-get -y check || exit 1
